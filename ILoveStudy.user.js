30351d321bc7599850fe740819f68e208d453c35
Yandong Liu <yandong.liu1996@gmail.com>
2017-03-23 12:07:01 -0700

    Create ILoveStudy.user.js

---
+++ ILoveStudy.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        ILoveStudy
+// @namespace   Y L
+// @include     *
+// @version     1
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
+if(document.title.match('.*(动画|漫画|小说|动漫).+')||(document.location.match('.*(%E5%8A%A8%E7%94%BB|%E6%BC%AB%E7%94%BB|%E5%B0%8F%E8%AF%B4|%E5%8A%A8%E6%BC%AB).+'))) {
+    console.log("match!")
+    document.write("呵呵，还看动漫小说，就这样放弃自己的梦想？赶紧滚去学习！")
+}
