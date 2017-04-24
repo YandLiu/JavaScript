df9227470e5375a5d2e265dab4c3bbfd2fd9ffc2
Yandong Liu <yandong.liu1996@gmail.com>
2017-04-24 15:54:30 -0700

    add support for bbs.gter.net

--- AutoCheckin.user.js
+++ AutoCheckin.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name        AutoCheckin
-// @namespace   namespace
+// @author      Y L
+// @namespace   https://github.com/YandLiu/Userscripts/
 // @include     https://www.repaik.com/plugin.php?id=dsu_paulsign:sign
 // @include     http://ezhanzy.com/thread-3083-1-1.html
 // @include     http://www.oneplusbbs.com/dsu_paulsign-sign.html
+// @include     http://bbs.gter.net*
 // @run-at      document-end
-// @version     1
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 
@@ -15,5 +17,15 @@ window.setTimeout( function () {
         document.querySelector("#todaysay").value = "嘻嘻哈哈";
         document.querySelector("#kx").click();
         showWindow('qwindow', 'qiandao', 'post', '0');
+    }
+    else if (ym == "bbs.gter.net") {
+        try {
+            showWindow('dsu_paulsign', 'plugin.php?id=dsu_paulsign:sign&show=sign&0f60c218');
+            document.querySelector('#eb321').click();
+            showWindow('qwindow', 'qiandao', 'post', '0');
+        }
+        catch (err) {
+            hideWindow('dsu_paulsign');
+        }
     }
 }, 2000);
