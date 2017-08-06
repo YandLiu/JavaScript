fc68e03abe2f98fe9955ef863f3724ff0a86c788
Yandong Liu <yandong.liu1996@gmail.com>
2017-08-06 08:42:06 +0800

    update

--- AutoCheckin.user.js
+++ AutoCheckin.user.js
@@ -9,20 +9,20 @@
 // @include     http://bbs.ntrqq.net*
 // @include     https://www.tailsocks.com/user/index.html
 // @include     http://ss.onekeyinstall.com/user/index.php
+// @include     http://bbs.kafan.cn/*
 // @run-at      document-end
-// @version     1.3
+// @version     1.4
 // @grant       none
 // ==/UserScript==
 
 var ym = document.location.hostname;
-window.setTimeout( function () {
+window.setTimeout(function() {
     if (ym == "www.repaik.com" || ym == "ezhanzy.com" || ym == "www.oneplusbbs.com" || ym == 'bbs.ntrqq.net') {
         document.querySelector("#todaysay").value = "嘻嘻哈哈";
         document.querySelector("#fd").click();
         try {
             document.querySelector('button.pn.pnc').click();
-        }
-        catch (err) {
+        } catch (err) {
             document.querySelector("a[onclick=\"showWindow('qwindow', 'qiandao', 'post', '0');return false\"]").click();
         }
         // showWindow('qwindow', 'qiandao', 'post', '0');
@@ -32,12 +32,15 @@ window.setTimeout( function () {
             showWindow('dsu_paulsign', 'plugin.php?id=dsu_paulsign:sign&show=sign&0f60c218');
             document.querySelector('#eb321').click();
             showWindow('qwindow', 'qiandao', 'post', '0');
-        }
-        catch (err) {
+        } catch (err) {
             hideWindow('dsu_paulsign');
         }
     }
-    if (ym == "www.tailsocks.com" || ym == "ss.onekeyinstall.com") {
-            document.querySelector('.btn.btn-success').click();
+    if (ym == "www.tailsocks.com" || ym == "ss.onekeyinstall.com")
+        document.querySelector('.btn.btn-success').click();
+    if (ym == 'bbs.kafan.cn') {
+        let el = document.querySelector('.qq_bind');
+        if (el.src != 'http://a.ikafan.com//plugin/dsu_amupper/images/wb.png')
+            el.click();
     }
 }, 2000);
