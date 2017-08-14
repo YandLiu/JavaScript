997578a919eefb94e6537c72582e7eb82f5c5316
Yandong Liu <yandong.liu1996@gmail.com>
2017-08-14 11:11:38 +0800

    update

--- Zhidao_Auto_Expand.user.js
+++ Zhidao_Auto_Expand.user.js
@@ -3,9 +3,16 @@
 // @author      Y L
 // @namespace   https://github.com/YandLiu/Userscripts/
 // @match       https://zhidao.baidu.com/*
-// @version     1.1
+// @version     1.4
 // @grant       none
 // ==/UserScript==
-document.querySelector('#show-answer-hide').click();
-var expandList = document.querySelectorAll('a.expend');
-for(var i of expandList) i.click();
+try {
+    document.querySelector('#show-answer-hide').click();
+} catch (e) {}
+try {
+    var expandList = document.querySelectorAll('a.expend');
+    for (var i of expandList) i.click();
+} catch (e) {}
+try {
+    document.querySelector('.show-hide-dispute').click();
+} catch (e) {}
