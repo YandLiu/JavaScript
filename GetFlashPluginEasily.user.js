fe192741a2e362b0281cc5d664b49818c682e8d6
Yandong Liu <yandong.liu1996@gmail.com>
2017-09-01 22:49:35 +0800

    update

--- GetFlashPluginEasily.user.js
+++ GetFlashPluginEasily.user.js
@@ -1,43 +1,26 @@
 // ==UserScript==
 // @name        GetFlashPluginEasily
-// @namespace   https://bitbucket.org/AlwaysWithYou/
-// @description add a link to download the whole flash player install package
-// @include     https://get.adobe.com/*/flashplayer/*
-// @include     https://get2.adobe.com/*/flashplayer/*
-// @include     https://get3.adobe.com/*/flashplayer/*
-// @run-at      document-end
-// @version     1
+// @namespace   https://github.com/YandLiu/Userscripts/
+// @description Add links to download adobe flash player directely
+// @include     /^https://get(\d)?\.adobe\.com/([^/]+/)?flashplayer/.*/
+// @run-at      document-idle
+// @version     1.0
 // @grant       none
 // ==/UserScript==
-//https://fpdownload.macromedia.com/get/flashplayer/current/licensing/win/install_flash_player_19_active_x.exe
-//https://fpdownload.macromedia.com/get/flashplayer/current/licensing/win/install_flash_player_19_plugin.exe
-var debugnpapilink = document.createElement("a");
+
 var npapilink = document.createElement("a");
-var debugielink = document.createElement("a");
 var ielink = document.createElement("a");
-debugnpapilink.innerHTML = "latest debug version for other browsers<br/><br/>";
-npapilink.innerHTML = "latest version for other browsers<br/><br/>";
-debugielink.innerHTML = "latest debug version for IE<br/><br/>";
-ielink.innerHTML = "latest version for IE<br/><br/>";
-window.setTimeout(
-    function(){
-        var olink = document.getElementById('buttonDownload');
-        debugnpapilink.href = olink.href.replace(/^.+installer=FP_(\d{2})_.+/, "http://download.macromedia.com/pub/flashplayer/updaters/$1/flashplayer_$1_plugin_debug.exe");
-        npapilink.href = olink.href.replace(/^.+installer=FP_(\d{2})_.+/, "https://fpdownload.macromedia.com/get/flashplayer/current/licensing/win/install_flash_player_$1_plugin.exe");
-        debugielink.href = olink.href.replace(/^.+installer=FP_(\d{2})_.+/, "http://download.macromedia.com/pub/flashplayer/updaters/$1/flashplayer_$1_ax_debug.exe");
-        ielink.href = olink.href.replace(/^.+installer=FP_(\d{2})_.+/, "https://fpdownload.macromedia.com/get/flashplayer/current/licensing/win/install_flash_player_$1_active_x.exe");
-        olink.parentNode.parentNode.insertBefore(npapilink, olink.parentNode);
-        olink.parentNode.parentNode.insertBefore(ielink, olink.parentNode);
-        olink.parentNode.parentNode.insertBefore(debugnpapilink, olink.parentNode);
-        olink.parentNode.parentNode.insertBefore(debugielink, olink.parentNode);
-    },1000
-);
+var ppapilink = document.createElement("a");
 
+npapilink.innerHTML = "latest NPAPI version<br/><br/>";
+ielink.innerHTML = "latest ActiveX version<br/><br/>";
+ppapilink.innerHTML = "latest PPAPI version<br/><br/>";
 
-// 另一种方法
-// window.addEventListener(
-//     "onload",
-//     window.setTimeout(function(){
-//         alert(document.getElementById("buttonDownload").href);
-//     }, 650)
-// );
+npapilink.href = 'https://fpdownload.macromedia.com/pub/flashplayer/latest/help/install_flash_player.exe';
+ielink.href = 'https://fpdownload.macromedia.com/pub/flashplayer/latest/help/install_flash_player_ax.exe';
+ppapilink.href = 'https://fpdownload.macromedia.com/pub/flashplayer/latest/help/install_flash_player_ppapi.exe';
+
+var olink = document.getElementById('AUTO_ID_columnright_p_note');
+olink.parentNode.insertBefore(npapilink, olink);
+olink.parentNode.insertBefore(ielink, olink);
+olink.parentNode.insertBefore(ppapilink, olink);
