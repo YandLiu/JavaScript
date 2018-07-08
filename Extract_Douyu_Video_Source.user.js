01991c17a66e9ec1899c369e8e3c2b1fc22324c0
YandLiu <yandong.liu1996@gmail.com>
2018-07-08 18:14:34 +0800

    update

--- Extract_Douyu_Video_Source.user.js
+++ Extract_Douyu_Video_Source.user.js
@@ -1,3 +1,11 @@
+// ==UserScript==
+// @name Extract Douyu Video Source
+// @namespace https://github.com/YandLiu
+// @author YandLiu
+// @match https://vmobile.douyu.com/*
+// @grant none
+// ==/UserScript==
+
 var video = document.querySelector('#video');
 var source = video.src;
 document.querySelector('.video-describe-box').innerHTML = '<a href="' + source + '">' + source + '</a>';
