52d7f3ab1cc2e5c11ba773abaa5549a5bac25347
YandLiu <yandong.liu1996@gmail.com>
2019-07-15 18:02:55 +1000

    add

---
+++ ChangeTitlesOfWeChatSubscriptionArticles.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Change Titles Of WeChat Subscription Articles
+// @description
+// @namespace   Violentmonkey Scripts
+// @match       https://mp.weixin.qq.com/s
+// @grant       none
+// ==/UserScript==
+
+document.title = document.querySelector('#activity-name').innerHTML;
