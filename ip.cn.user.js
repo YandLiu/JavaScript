7c5c39aa183f70b34da9f5ad6cf580f58f2ab55d
YandLiu <yandong.liu1996@gmail.com>
2019-08-18 16:51:34 +1000

    ip.cn

---
+++ ip.cn.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name ip.cn
+// @namespace Violentmonkey Scripts
+// @match https://ip.cn/
+// @grant none
+// ==/UserScript==
+
+document.querySelector('img#tp').setAttribute('width', '200px');
